4e7a8ba4346c26b06e25bb2fa2b73d356baddefb
mohitdhiman <mohitdhiman@EXO-GGN-0324.local>
2022-04-05 21:51:26 +0530

    interactive utils changes

--- tmux/mac/scripts/interactiveUtils.zsh
+++ tmux/mac/scripts/interactiveUtils.zsh
@@ -1,3 +1,4 @@
 #!/bin/zsh
-output=`$1 list-options |fzf-tmux -r 20% --multi --reverse| $1`
+output=`$1 list-options |fzf-tmux -r 20% --multi --reverse`
+output=`$1 $output`
 tmux send-keys "$output"
