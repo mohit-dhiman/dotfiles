0a6b7f87b324fb9c84f11eff40073d8998949157
Mohit Dhiman <21983488+mohit-dhiman@users.noreply.github.com>
2025-11-04 23:25:46 +0530

    more custom alias

--- zsh/.customalias.zsh
+++ zsh/.customalias.zsh
@@ -1,6 +1,11 @@
 alias cp="cp -i"
 alias cds="cd /Users/mohitdhiman/Workspaces"
+alias cdc="cd /Users/mohitdhiman/Workspaces/carsales"
 alias cdd="cd /Users/mohitdhiman/Documents"
 alias cdw="cd /Users/mohitdhiman/Downloads"
 tmx() { tmux a -t Endor || tmux new -t Mac -s Endor}
 alias grep="ggrep --color=auto"
+alias tf="terraform"
+alias ll="ls -ltrh"
+alias jqp='pbpaste | jq .'
+alias jqc='pbpaste | jq . | tee >(pbcopy)'
